691094e3000b3ef915e837981912fc603e5008de
Ron Soeum <ronsoeum@gmail.com>
2019-02-06 11:59:17 -0600

    renamed files

--- src/main/java/AdsIndexServlet.java
+++ src/main/java/AdsIndexServlet.java
@@ -6,8 +6,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(name = "ViewAdsServlet", urlPatterns = "/ads")
-public class ViewAdsServlet extends HttpServlet {
+@WebServlet(name = "AdsIndexServlet", urlPatterns = "/ads")
+public class AdsIndexServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
