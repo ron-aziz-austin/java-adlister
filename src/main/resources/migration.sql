06f07bf3491f61de32733c516d96c5b6bdb1da5e
Ron Soeum <ronsoeum@gmail.com>
2019-02-08 10:28:21 -0600

    added create_database.sql, and changed database user

--- src/main/resources/migration.sql
+++ src/main/resources/migration.sql
@@ -1,12 +1,8 @@
--- database: adlister_db
--- user: user
--- password: password
-
 USE adlister_db;
 
 DROP TABLE IF EXISTS users;
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
@@ -18,7 +14,7 @@ CREATE TABLE users (
 
 DROP TABLE IF EXISTS ads;
 
-CREATE TABLE ads (
+CREATE TABLE IF NOT EXISTS ads (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user_id INT UNSIGNED NOT NULL,
     title VARCHAR(255) NOT NULL,
