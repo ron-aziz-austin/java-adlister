17d4dd96df71e59ea29fd191ce9bce2a9f901679
Austin <Austin.r.garza@gmail.com>
2019-02-13 15:07:10 -0600

    added to users table

--- src/main/resources/migration.sql
+++ src/main/resources/migration.sql
@@ -1,18 +1,35 @@
 USE adlister_db;
 
+
+SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS users;
+# TRUNCATE users;
+SET FOREIGN_KEY_CHECKS = 1;
 
 CREATE TABLE IF NOT EXISTS users (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    first_name CHAR(150) NOT NULL,
+    last_name CHAR(150) NOT NULL,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
+    join_date DATE NOT NULL,
+    posts INT UNSIGNED NOT NULL,
+    website VARCHAR(255),
+    phone_number VARCHAR(10) NOT NULL CHECK (phone_number between 0 and 9999999999),
+    user_bio VARCHAR(350), # maybe make this varchar(x)?
+    user_fb varchar(250),
+    user_twitter varchar(250),
+    user_linkedin varchar(250),
     PRIMARY KEY (id),
     UNIQUE (username),
     UNIQUE (email)
 );
 
+SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS ads;
+# TRUNCATE ads;
+SET FOREIGN_KEY_CHECKS = 1;
 
 CREATE TABLE IF NOT EXISTS ads (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
