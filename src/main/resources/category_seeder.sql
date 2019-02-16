f7a630b6c07c7cc21e7fc6c2eb41174a2ff81515
Ron Soeum <ronsoeum@gmail.com>
2019-02-15 21:40:03 -0600

    changed parent_id values for super categories to = 0

--- src/main/resources/category_seeder.sql
+++ src/main/resources/category_seeder.sql
@@ -5,12 +5,12 @@ TRUNCATE category;
 SET FOREIGN_KEY_CHECKS = 1;
 
 INSERT INTO category (parent_id, title)
-VALUES (null, 'Kids Products'),
-       (null, 'Entertainment'),
-       (null, 'Camera'),
-       (null, 'Computers & Laptops'),
-       (null, 'Fashion & Beauty'),
-       (null, 'Jobs'),
+VALUES (0, 'Kids Products'),
+       (0, 'Entertainment'),
+       (0, 'Camera'),
+       (0, 'Computers & Laptops'),
+       (0, 'Fashion & Beauty'),
+       (0, 'Jobs'),
        (1, 'Clothes & Footwear'),
        (2, 'Books'),
        (3, 'Canon'),
