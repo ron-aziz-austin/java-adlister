7a83e4ac275182222ef49bc16846bb375d0cafb2
Ron Soeum <ronsoeum@gmail.com>
2019-02-19 16:03:55 -0600

    cleaned code for efficiency

--- src/main/java/com/codeup/adlister/controllers/AdPageServlet.java
+++ src/main/java/com/codeup/adlister/controllers/AdPageServlet.java
@@ -1,46 +1,33 @@
 package com.codeup.adlister.controllers;
-
 import com.codeup.adlister.dao.DaoFactory;
 import com.codeup.adlister.models.Ad;
-import com.codeup.adlister.models.User;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
-
 
 @WebServlet (name = "AdPageServlet", urlPatterns = "/adIndex")
 public class AdPageServlet extends HttpServlet {
-    @Override
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        List<Ad> ads = DaoFactory.getAdsDao().all();
 
+        // get ad id from URL query
         int adId = Integer.parseInt(request.getParameter("adId"));
-        Ad ad = ads.get(adId-1);
-        request.setAttribute("ad", adId);
+        // find ad by id and create an ad object
+        Ad ad = DaoFactory.getAdsDao().findById(adId);
+        // send ad object to html
+        request.setAttribute("ad", ad);
+        // send list of categories to html
+        request.setAttribute("categories", DaoFactory.getCategoriesDao().all());
 
+        // TODO create a classified_status interface, class, dao, etc ... possibly get rid of this column in the ads table
+        //request.setAttribute("status", staus);
 
+        // send user to the individual ad page
         request.getRequestDispatcher("/WEB-INF/ads/ad_page.jsp").forward(request,response);
 
-    }
+    }//doGet
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-
-
-
-
-
-
-
-    }
-
-    public static String getUserId(User user){
-        return user.getId() + "";
-    }
-}
+}// class
