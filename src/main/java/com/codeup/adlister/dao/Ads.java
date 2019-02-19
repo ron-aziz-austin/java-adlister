59361347aacc7fdb0cb7133cda4b374d64cbf854
Ron Soeum <ronsoeum@gmail.com>
2019-02-19 16:05:20 -0600

    added methods in interface

--- src/main/java/com/codeup/adlister/dao/Ads.java
+++ src/main/java/com/codeup/adlister/dao/Ads.java
@@ -7,4 +7,6 @@ public interface Ads {
     List<Ad> all();
     // insert a new ad and return the new ad's id
     Long insert(Ad ad);
+    Ad findById(int id);
+    List<Ad> listByCategory(Long input);
 }
