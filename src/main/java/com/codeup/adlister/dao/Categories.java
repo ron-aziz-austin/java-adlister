5d658f83a2b06818cc2eae11c0f7ab8beea5b02b
Ron Soeum <ronsoeum@gmail.com>
2019-02-19 16:11:41 -0600

    comment out the findAdCategory method

--- src/main/java/com/codeup/adlister/dao/Categories.java
+++ src/main/java/com/codeup/adlister/dao/Categories.java
@@ -3,6 +3,6 @@ import com.codeup.adlister.models.Category;
 import java.util.List;
 
 public interface Categories {
-    Category findAdCategory(Long categoryId);
+//    Category findAdCategory(Long categoryId);
     List<Category> all();
 }
