8da92bb46f4c302ec918f0c30e8985a1c95b8462
Ron Soeum <ronsoeum@gmail.com>
2019-02-19 12:10:13 -0600

    added code to retrieve ad id from the URL query

--- src/main/java/com/codeup/adlister/controllers/AdPageServlet.java
+++ src/main/java/com/codeup/adlister/controllers/AdPageServlet.java
@@ -1,5 +1,7 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
 import com.codeup.adlister.models.User;
 
 import javax.servlet.ServletException;
@@ -8,20 +10,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+import java.util.List;
 
 
 @WebServlet (name = "AdPageServlet", urlPatterns = "/adIndex")
 public class AdPageServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/ads/ad_page.jsp").forward(req,resp);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Ad> ads = DaoFactory.getAdsDao().all();
+
+        int adId = Integer.parseInt(request.getParameter("adId"));
+        Ad ad = ads.get(adId-1);
+        request.setAttribute("ad", adId);
+
+
+        request.getRequestDispatcher("/WEB-INF/ads/ad_page.jsp").forward(request,response);
 
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+
+
+
+
+
 
     }
 
