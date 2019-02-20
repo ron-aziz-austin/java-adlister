f901279d6658465c28fad64746bc57c220e82e1b
Ron Soeum <ronsoeum@gmail.com>
2019-02-20 09:22:28 -0600

    search bar now searches by title; select option works with parent category and sub category

--- src/main/java/com/codeup/adlister/controllers/AdsIndexServlet.java
+++ src/main/java/com/codeup/adlister/controllers/AdsIndexServlet.java
@@ -17,9 +17,25 @@ public class AdsIndexServlet extends HttpServlet {
     }// doGet
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        Long category = Long.parseLong(request.getParameter("category"));
-        request.removeAttribute("ads");
-        request.setAttribute("ads", DaoFactory.getAdsDao().listByCategory(category));
+
+        if (request.getParameter("category") != null) {
+            Long category = Long.parseLong(request.getParameter("category"));
+            if (Long.parseLong(request.getParameter("category")) >= 1 && Long.parseLong(request.getParameter("category")) <=6) {
+                request.removeAttribute("ads");
+                request.setAttribute("ads", DaoFactory.getAdsDao().listByParentCategory(category));
+            } else {
+                request.removeAttribute("ads");
+                request.setAttribute("ads", DaoFactory.getAdsDao().listByCategory(category));
+            }
+        }// if
+
+        if (request.getParameter("search") != null) {
+            String search = request.getParameter("search");
+            request.removeAttribute("ads");
+            request.setAttribute("ads", DaoFactory.getAdsDao().listByTitle(search));
+        }// if
+
+
         request.setAttribute("categories", DaoFactory.getCategoriesDao().all());
         request.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(request, response);
     }
