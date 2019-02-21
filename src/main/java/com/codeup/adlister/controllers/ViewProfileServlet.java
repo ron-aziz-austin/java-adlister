73e11db228b35f002f3e129ff687720a0e49d531
Ron Soeum <ronsoeum@gmail.com>
2019-02-21 14:39:36 -0600

    added logic to delete a user's ad

--- src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
+++ src/main/java/com/codeup/adlister/controllers/ViewProfileServlet.java
@@ -1,5 +1,6 @@
 package com.codeup.adlister.controllers;
 import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
 import com.codeup.adlister.models.User;
 
 import javax.servlet.ServletException;
@@ -13,16 +14,20 @@ import java.io.IOException;
 public class ViewProfileServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         if (request.getSession().getAttribute("user") == null) {
             response.sendRedirect("/login");
             return;
         }
-
         User loginUser = (User) request.getSession().getAttribute("user");
+        if (request.getParameter("deleteAd") != null) {
+            // get ad id from URL query
+            Long deleteAd = Long.parseLong(request.getParameter("deleteAd"));
+            // find ad by id and create an ad object
+            DaoFactory.getAdsDao().delete(deleteAd);
+        }
         request.setAttribute("ads", DaoFactory.getAdsDao().listAdsByUserId(loginUser.getId()));
-
         request.setAttribute("categories", DaoFactory.getCategoriesDao().all());
-
         request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
     }// doGet
 
