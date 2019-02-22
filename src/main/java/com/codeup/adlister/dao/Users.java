2aaa40c4aa89174d7ec9d1dbc62771b1e3ee5131
Ron Soeum <ronsoeum@gmail.com>
2019-02-21 18:14:00 -0600

    added method to interface

--- src/main/java/com/codeup/adlister/dao/Users.java
+++ src/main/java/com/codeup/adlister/dao/Users.java
@@ -1,7 +1,9 @@
 package com.codeup.adlister.dao;
 import com.codeup.adlister.models.User;
+import java.util.List;
 
 public interface Users {
+    List<User> usersPublicInfo();
     User findByUsername(String username);
     User findByEmail(String email);
     Long insert(User user);
